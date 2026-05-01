44b3fb890475ef9c6f8c55f545d068fba8d29482
Khalid <khalidsyfullah326@gmail.com>
2026-05-01 20:13:53 +0600

    My java project

---
+++ src/SwitchStatement.java
@@ -0,0 +1,18 @@
+public class SwitchStatement {
+    static void main(String[] args)
+    {
+        String day = "Friday";
+        switch (day)
+        {
+            case "Sunday" :
+                System.out.println("Its Sunday");
+                break;
+            case "Monday" :
+                System.out.println("Its Monnday");
+                break;
+            case "Friday" :
+                System.out.println("Its Friday");
+                break;
+        }
+    }
+}
